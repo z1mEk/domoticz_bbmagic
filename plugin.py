f36133ca87ae17b4a9e906265df4244a2baff0f3
Gabriel <gabriel.zima@gmail.com>
2018-07-04 12:41:45 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -1,1 +1,16 @@
+"""
+<plugin key="BBMagic" name="BBMagic BLE" author="z1mEk" version="1.0.0" wikilink="" externallink="https://http://bbmagic.net/">
+    <description>
+
+    </description>
+    <params>
+        <param field="Mode6" label="Debug" width="75px">
+            <options>
+                <option label="True" value="Debug"/>
+                <option label="False" value="Normal"  default="true" />
+            </options>
+        </param>
+    </params>
+</plugin>
+"""
 
